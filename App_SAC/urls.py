9ccadf286239c793843b9b35246e67c8b0fd1990
ViniTex231 <viniteixeira2828@gmail.com>
2023-08-18 11:00:33 -0300

    first

---
+++ App_SAC/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.abre_index, name='abre_index'),
+    path('cad_cliente', views.cad_cliente, name='cad_cliente'),
+    path('salvar_cliente_novo', views.salvar_cliente_novo, name='salvar_cliente_novo'),
+    path('cons_cliente', views.cons_cliente, name='cons_cliente'),
+    path('edit_cliente/<int:id>', views.edit_cliente, name='edit_cliente'),
+    path('salvar_cliente_editado', views.salvar_cliente_editado, name='salvar_cliente_editado'),
+    path('excluir_cliente/<int:id>', views.delete_cliente, name='delete_cliente'),
+]
